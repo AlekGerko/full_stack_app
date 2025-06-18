3bf648d72f842f6219557fc227a16ff947631e0e
Alek Gerko <alek.gerko@gmail.com>
2025-06-18 07:04:08 +0300

    lesson 6

--- server/index.js
+++ server/index.js
@@ -1,19 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
 
-const db = require('./models');
+const db = require("./models");
 
 //Routers
-const postRouter = require('./routes/Posts');
-app.use('/posts', postRouter);
-
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+const commentRouter = require("./routes/Comments");
+app.use("/comments", commentRouter);
 
 db.sequelize.sync().then(() => {
-    app.listen(3001, () => {
-        console.log("3001 server");
-    });
+  app.listen(3001, () => {
+    console.log("3001 server");
+  });
 });
---
+++ server/routes/Comments.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const { Comments } = require("../models");
+
+// router.get("/", async (req, res) => {
+//   // res.send("works"); //to return html
+//   // res.json("works");
+//   const listOfComments = await Comments.findAll();
+//   res.json(listOfComments);
+// });
+
+router.get("/:postId", async (req, res) => {
+  const postId = req.params.postId;
+  const comment = await Comments.findAll({ where: { PostId: postId } });
+
+  res.json(comment);
+});
+
+router.post("/", async (req, res) => {
+  const comment = req.body; //take data from request (body)
+  await Comments.create(comment); //crete comment
+
+  res.json(comment); //result (needed to all work, like return)
+});
+
+module.exports = router;
